edd4efa24b933c7c2d901f5df130ab45ff4cda5c
Luiz Fernando <luizfernandofernandes.lf@gmail.com>
2019-05-02 14:18:34 -0300

    🚀 initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.0.0/images/marker-icon.png',
+  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.0.0/images/marker-icon.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.0.0/images/marker-shadow.png',
+});
+
+class App extends React.Component {
+
+  state = INITIAL_STATE;
+
+  componentWillMount() {
+    this._setLocation();
+  }  
+
+  _setLocation(lat = -22.9034, lng = -43.1917) {
+    this.setState({...this.state, lat, lng});
+  }
+
+  render() {
+    const position = [this.state.lat, this.state.lng]
+    return (
+        <Map  style={MapContainerStyle} center={position} zoom={this.state.zoom}>
+          <TileLayer
+            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={position}>
+            <Popup>
+              Endereço
+            </Popup>
+          </Marker>
+        </Map>
+    )
+  }
+}
+
+const MapContainerStyle = {
+  width: '100%',
+  height: '800px'
+};
+
+const INITIAL_STATE = {
+    lat: 0,
+    lng: 0,
+    zoom: 7
 }
 
 export default App;
